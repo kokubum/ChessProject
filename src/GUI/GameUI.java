685e89dc73c7b0fbfd39106c6aa8eb183ebeb62e
AlbertoDoc <alberto-oliveira-santos@hotmail.com>
2019-11-08 16:57:22 -0300

    Adição do esboço de uma interface

---
+++ src/GUI/GameUI.java
@@ -0,0 +1,51 @@
+package GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import game.*;
+
+public class GameUI extends JFrame {
+
+	private JPanel contentPane;
+
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GameUI frame = new GameUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	//Sobrecarga no construtor para ser utilizado no tratamento de exceções
+	public GameUI() {
+		setTitle("ChessProject - Game");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 0, 1200, 700);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+
+	//Construtor para abrir a tela principal do jogo
+	public GameUI(String nickName1, boolean isWhite1, String nickName2, boolean isWhite2) {
+		setTitle("ChessProject - Game");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 0, 1200, 700);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+	}
+
+}
